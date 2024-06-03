aa53d34e336466ea583cc11752e6aab4dc82da75
tron0318 <tron0318@naver.com>
2024-06-03 14:15:01 +0900

    setting

---
+++ src/main/java/com/example/Chat_2024_06_03/Chat20240603Application.java
@@ -0,0 +1,13 @@
+package com.example.Chat_2024_06_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat20240603Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat20240603Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Chat_2024_06_03/Chat20240603ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chat_2024_06_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat20240603ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
