8da0aef2934e7f0febbe0e720de93bc371058254
tron0318 <tron0318@naver.com>
2024-06-03 14:44:32 +0900

    RequestParam value, defaultValue

--- src/main/java/com/example/Chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
+++ src/main/java/com/example/Chat_2024_06_03/domain/chat/chatRoom/controller/chatRoomController.java
@@ -3,14 +3,20 @@ package com.example.Chat_2024_06_03.domain.chat.chatRoom.controller;
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class chatRoomController {
-    @GetMapping("/chat/room/1")
+    @GetMapping("/chat/room/{roomId}")
     @ResponseBody
-    public String showRoom(){
-        return "1번 채팅방입니다.";
+    public String showRoom(
+            @PathVariable("roomId") final long roomId,
+            @RequestParam(value = "writerName",defaultValue = "NoName") final String writerName
+
+    ){
+        return "%d번 채팅방입니다. writer : %s".formatted(roomId, writerName);
 
     }
 
